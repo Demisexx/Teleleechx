e2e0483500247fb28f586b9a9193895168812670
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-11 07:30:42 +0530

    Update __init__.py

--- tobrot/bot_theme/languages/__init__.py
+++ tobrot/bot_theme/languages/__init__.py
@@ -1,1 +1,11 @@
-# Start Code Soon !!
+from tobrot import LOGGER, BOT_LANG
+from tobrot.bot_theme.languages import en, bn
+
+AVAILABLE_LANG = {'english': en, 'bengali': bn}
+
+def BotLang():
+
+    if BOT_LANG in AVAILABLE_LANG.keys():
+        return (AVAILABLE_LANG.get(BOT_LANG)).TXLanguage()
+    else:
+        return en.TXLanguage()
